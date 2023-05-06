ee230159adacfa3ec66781f454ce93a38ad3914a
Kristel <kristelclaire.02@gmail.com>
2023-05-06 16:48:59 +0100

    Add a function to resize the grid
    Add functions to create a new grid whenever the user wants.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('button');
 
-function createGrid(row, col) {
-    container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+btn.addEventListener('click', changeGrid);
 
-    for (let i = 0; i < (row * col); i++) {
+
+function createGrid(num) {
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+
+    for (let i = 0; i < (num ** 2); i++) {
         let div = document.createElement('div');
         div.classList.add('cell');
         container.appendChild(div);
@@ -12,10 +16,10 @@ function createGrid(row, col) {
     }
 }
 
-createGrid(16, 16);
+createGrid(16);
+
 
 const cell = document.querySelectorAll('.cell');
-
 
 const cellHovered = e => {
     cellId = e.target.id;
@@ -26,8 +30,24 @@ for (let gridItem of cell) {
     gridItem.addEventListener('mouseover', cellHovered);
 }
 
-
 function changeCellColor() {
-    console.log(cellId);
-    document.getElementById(`${cellId}`).classList.add('colored');
+    document.getElementById(`${cellId}`).style.backgroundColor = 'black';   
+}
+
+function resetGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+}
+
+function changeGrid() {
+    let size = parseInt(prompt('Insert the size of the grid', ''));
+
+    if (size > 0 && size <= 100) {
+        resetGrid();
+        createGrid(size);
+    } else {
+        alert('Not valid. Retry!')
+    }
+        
 }
