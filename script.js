a647cacd58433ff6a088536ef2c0fbd45a4524aa
Kristel <kristelclaire.02@gmail.com>
2023-05-06 15:00:24 +0100

    Set hover effect
    Add event listener on hover that changes the background color of the cell
    when hovered.

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const container = document.querySelector('.container');
-const cell = document.querySelectorAll('.cell');
 
 function createGrid(row, col) {
     container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
 
-    for (let i = 0; i < (row * col) -1 ; i++) {
+    for (let i = 0; i < (row * col); i++) {
         let div = document.createElement('div');
         div.classList.add('cell');
         container.appendChild(div);
+        div.setAttribute('id', `cell-${i + 1}`)
     }
 }
 
-function name(params) {
-  
+createGrid(16, 16);
+
+const cell = document.querySelectorAll('.cell');
+
+
+const cellHovered = e => {
+    cellId = e.target.id;
+    changeCellColor();
 }
 
-createGrid(16, 16);
+for (let gridItem of cell) {
+    gridItem.addEventListener('mouseover', cellHovered);
+}
+
+
+function changeCellColor() {
+    console.log(cellId);
+    document.getElementById(`${cellId}`).classList.add('colored');
+}
